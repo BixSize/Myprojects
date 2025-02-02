24ba8e758b14a35d700e9cf497c7f7f8eed90f46
BixSize <ulukmansamatov@gmail.com>
2025-02-03 05:11:54 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// who's buying Lunch Code Chalange
+
+function whosPaying(names) {
+    var numberOfPeople = names.length;
+    length
+    var randomPersonPosition = Math.floor(Math.random() * numberOfPeople);
+
+    var randomPerson = names[randomPersonPosition];
+    return randomPerson + " is going to buy lunch today!";
+}
